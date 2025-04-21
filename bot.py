42819f354e62f60b8ecd7687f19edc124bea6236
ArgenisTV <argenistvq@gmail.com>
2025-04-20 21:33:37 -0600

    Small update
    Added FuzzyWuzzy library to help find the best match for a song's name.
    
    Added play functionality and the ability to join/move voice channels.
    
    Bot will now send a comment while searching for a song.
    
    Added options for the audio stream.
    
    Added logic for the 'entry' to be compared against results and find the best match.
    
    Added logic for audio to be reproduced through FFmpeg.

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import os
 import discord
+import yt_dlp # Extracts info and URLs from Youtube
 from discord import FFmpegPCMAudio
-import yt_dlp 
 from discord.ext import commands # Allows commands creation
 from dotenv import load_dotenv # Just loads variables from the .env file
+from fuzzywuzzy import fuzz # Used to get the best match for a song's name
 
 load_dotenv() # Loads environment variables
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -18,9 +19,60 @@ async def on_ready():
     print(f"{bot.user} Be not afraid!")
 
 @bot.command()
-async def ping(ctx): # Mainly a test command. CTX is the command context, such as who wrote, on which channel, etc.
-    await ctx.send("Use me, will you?") 
+async def ping(ctx): 
+    await ctx.send("Use me, will you?") # Mainly a test command. CTX is the command context, such as who wrote, on which channel, etc.
+
+@bot.command()
+async def play(ctx, url: str): # What happens when the bot recognizes the play command.
+    voice_channel = ctx.author.voice.channel if ctx.author.voice else None # Checks if the user is in a voice channel.
+
+    if not voice_channel: 
+        await ctx.send("Joined a voice channel, you have not.")
+        return # Throws error message if user is not in a voice channel.
+
+    # Connecting to a voice channel
+    voice_client = ctx.voice_client
+    if not voice_client:
+        voice_client = await voice_channel.connect() # If not connected, joins the user's voice channel.
+    elif voice_client.channel != voice_channel:
+        await voice_client.move_to(voice_channel) # If connected to a different voice channel, moves to the user's.
+
+    await ctx.send(f"Thy choice shall be considered...") # Message shown while the bot is searching for the audio.
+
+    # Options for audio stream
+    ydl_opts ={
+        'format' : 'bestaudio/best',
+        'quiet': True, # No console logs
+        'noplaylist': True, 
+        'default_search': 'ytsearch', # Defaults to YouTube search if not using a URL.
+        'extract_flat': 'in_playlist',
+    }
+
+    # Video info is extracted
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False) 
+
+        # If multiple results are found, the closest match to the requested song's name is chosen.
+        if 'entries' in info:
+            best_match = None
+            highest_score = 0
+            for entry in info['entries']: # For each result from the given "URL" (song name), consider how much it matches with the URL and choose the one with the highest score.
+                title = entry.get('title', '').lower()
+                score = fuzz.partial_ratio(title, url.lower()) # From library FuzzyWuzzy. This measures the similarity between Strings from 0 to 100.
+                if score > highest_score: 
+                    best_match = entry
+                    highest_score = score
+            info = best_match
+
+    # Reproduce audio through FFmpeg
+    source = FFmpegPCMAudio(audio_url)
+    if voice_client.is_playing():
+        voice_client.add()
+    voice_client.play(source)
+    await ctx.send(f" Heed: **{title}**")
 
 # Execute bot
-bot.run(TOKEN) # 
+bot.run(TOKEN)
+
+
 
